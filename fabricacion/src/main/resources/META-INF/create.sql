2f86f3b2702e4c09930f3d87a143c44d800a2a98
jmrm92 <jmrm81992@gmail.com>
2023-10-06 13:23:24 +0200

    cambios

--- fabricacion/src/main/resources/META-INF/create.sql
+++ fabricacion/src/main/resources/META-INF/create.sql
@@ -6,7 +6,5 @@ CREATE TABLE fabricadb.fabricaciones (
 	coste_monetario DOUBLE NOT NULL,
 	coste_piezas INT NOT NULL,
 	cantidad_modelos INT NOT NULL,
-	CONSTRAINT fabricaciones_pk PRIMARY KEY (id_fabricacion),
-	CONSTRAINT fabricaciones_FK FOREIGN KEY (id_fabrica) REFERENCES fabricadb.fabricas(id_fabrica),
-	CONSTRAINT fabricaciones_FK_1 FOREIGN KEY (id_modelo) REFERENCES fabricadb.modelos(id_modelo)
+	CONSTRAINT fabricaciones_pk PRIMARY KEY (id_fabricacion)
 )
